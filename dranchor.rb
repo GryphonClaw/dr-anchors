e6d42e73dcc77634f9aee3a1d92ee72eedd10429
GryphonClaw <contact@gryphonclaw.net>
2025-04-30 00:53:46 -0700

    Corrected Anchor Points
    correct the anchor point constants.

--- dranchor.rb
+++ dranchor.rb
@@ -2,29 +2,29 @@
 # provides constants for changing the draw origin of a render primitive
 module Anchor
   # makes the draw origin of the sprite at the top left
-  TOP_LEFT = {anchor_x: -0.5, anchor_y: 1.0}.freeze
+  TOP_LEFT = {anchor_x: 0.0, anchor_y: 1.0}.freeze
 
   # makes the draw origin of the sprite at the top center
-  TOP_CENTER = {anchor_x: 0.5, anchor_y: 1.0}.freeze
+  TOP_CENTER = {anchor_x: -0.5, anchor_y: 1.0}.freeze
 
   # makes the draw origin of the sprite at the top right
-  TOP_RIGHT = {anchor_x: 1.0, anchor_y: 1.0}.freeze
+  TOP_RIGHT = {anchor_x: -1.0, anchor_y: 1.0}.freeze
 
   # makes the draw origin of the sprite at the center left
-  CENTER_LEFT = {anchor_x: -0.5, anchor_y: 0.0}.freeze
+  CENTER_LEFT = {anchor_x: 1.0, anchor_y: 0.0}.freeze
 
   # makes the draw origin of the sprite at the center
-  CENTER = {anchor_x: 0.5, anchor_y: 0.5}.freeze
+  CENTER = {anchor_x: 1.0, anchor_y: 0.5}.freeze
 
   # makes the draw origin of the sprite at the center right
-  CENTER_RIGHT = {anchor_x: 0.5, anchor_y: -0.5}.freeze
+  CENTER_RIGHT = {anchor_x: 1.0, anchor_y: -0.5}.freeze
   
   # makes the draw origin of the sprite at the bottom left (DragonRuby Default)
   BOTTOM_LEFT = {anchor_x: 0.0, anchor_y: 0.0}.freeze
 
-  # makes the draw origin of the sprite at the center right
-  BOTTOM_CENTER = {anchor_x: 0.5, anchor_y: -1.0}.freeze
+  # makes the draw origin of the sprite at the bottom center
+  BOTTOM_CENTER = {anchor_x: 0.5, anchor_y: 0.0}.freeze
   
-  # makes the draw origin of the sprite at the center right
-  BOTTOM_RIGHT= {anchor_x: 1.0, anchor_y: -1.0}.freeze
+  # makes the draw origin of the sprite at the bottom right
+  BOTTOM_RIGHT= {anchor_x: 1.0, anchor_y: 0.0}.freeze
 end
